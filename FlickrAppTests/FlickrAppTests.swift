fe5c8a776b64e231258398878880a30b355a4114
Muneer Kalakuzhiyil <muneerkk66@gmail.com>
2024-02-09 11:56:20 +0000

    chore: update app name

--- FlickrAppTests/FlickrAppTests.swift
+++ FlickrAppTests/FlickrAppTests.swift
@@ -8,7 +8,7 @@
 @testable import FlickrImage
 import XCTest
 
-final class FlickrImageTests: XCTestCase {
+final class FlickrAppTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
--- FlickrAppUITests/FlickrAppUITestsLaunchTests.swift
+++ FlickrAppUITests/FlickrAppUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class FlickrImageUITestsLaunchTests: XCTestCase {
+final class FlickrAppUITestsLaunchTests: XCTestCase {
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
